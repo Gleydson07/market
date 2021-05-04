6b7981071d75888a8542a0f1cce0727620ea145b
Gleydson Albuquerque <gassantos.dev@gmail.com>
2021-05-04 19:18:04 -0300

    modal with cicle button to cancel

--- scripts.js
+++ scripts.js
@@ -4,5 +4,12 @@ const DOM = {
             .getElementById('sidebar')
             .classList
             .toggle('off');
+    },
+
+    toggleModal(){
+        document
+            .querySelector('.container-modal')
+            .classList
+            .toggle('modal-active');
     }
 }
